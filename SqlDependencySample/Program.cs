26a80e1f2d85477b3e9e662aaf06edcc2c181beb
furuta tasuku <muddy-brain29@gmail.com>
2016-03-30 23:47:58 +0900

    初期状態

---
+++ SqlDependencySample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SqlDependencySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UnhandledExceptionイベントハンドラを追加
+            Thread.GetDomain().UnhandledException += new UnhandledExceptionEventHandler(UnhandledException);
+
+            var sample = new DependencySample();
+            Environment.Exit(0);
+        }
+
+        private static void UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            ErrorExit(e.ExceptionObject as Exception);
+        }
+
+        private static void ErrorExit(Exception ex)
+        {
+            Console.WriteLine("Error!!");
+            Environment.Exit(1);
+        }
+    }
+}
