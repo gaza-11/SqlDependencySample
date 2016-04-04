692b8e63dae089123e28e7a63eca9da0a46759a8
furuta tasuku <muddy-brain29@gmail.com>
2016-04-05 01:08:43 +0900

    処理追加

--- SqlDependencySample/DependencySample.cs
+++ SqlDependencySample/DependencySample.cs
@@ -1,16 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data.SqlClient;
+using System.Threading;
 
 namespace SqlDependencySample
 {
+    /// <summary>
+    /// SqlDependencyサンプルクラス
+    /// </summary>
     internal class DependencySample
     {
+        /// <summary>
+        /// ロック用オブジェクト
+        /// </summary>
+        private static object lockObj = new object();
+
+        /// <summary>
+        /// 接続文字列
+        /// </summary>
+        private string ConnectionString { get; set; }
+
+        /// <summary>
+        /// 最大ループ回数
+        /// </summary>
+        private int MaxLoopNum { get; set; }
+
+        /// <summary>
+        /// ループ停止時間（ミリ秒）
+        /// </summary>
+        private int SleepMillisec { get; set; }
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
         internal DependencySample()
         {
+            this.ConnectionString ="";
+            this.MaxLoopNum = 60;
+            this.SleepMillisec = 1000;
+        }
 
+        /// <summary>
+        /// メインループ処理
+        /// </summary>
+        public void Loop()
+        {
+            var loopCount = 0;
+            while (true)
+            {
+                Thread.Sleep(1000);
+                loopCount++;
+                if (loopCount >= this.MaxLoopNum)
+                {
+                    this.DoSomething();
+                }
+
+                //// ループの停止処理をここに記載
+            }
+        }
+
+        /// <summary>
+        /// SqlDependency初期化
+        /// </summary>
+        private void Initialize()
+        {
+            SqlDependency.Start(this.ConnectionString);
+        }
+
+        /// <summary>
+        /// 実処理をここに記載します。
+        /// </summary>
+        private void DoSomething()
+        {
+            
         }
     }
 }
