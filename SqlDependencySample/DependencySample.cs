f5ea4db3466508dd5b6e6689b9efd7a137104067
furuta tasuku <muddy-brain29@gmail.com>
2016-04-06 01:54:18 +0900

    SqlDependency関連処理記述。

--- SqlDependencySample/DependencySample.cs
+++ SqlDependencySample/DependencySample.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.SqlClient;
+﻿using System;
+using System.Data.SqlClient;
 using System.Threading;
 
 namespace SqlDependencySample
@@ -27,6 +28,27 @@ namespace SqlDependencySample
         /// ループ停止時間（ミリ秒）
         /// </summary>
         private int SleepMillisec { get; set; }
+
+        /// <summary>
+        /// 監視設定SQL
+        /// </summary>
+        /// <remarks>
+        /// テーブル名はスキーマ名から記述する必要あり。
+        /// 検索カラムの*による省略不可。
+        /// 
+        /// 監視対象テーブルを複数のプロセスが1トランザクション内で複数回更新する場合、
+        /// 監視専用のテーブルを別に用意したほうが良い。
+        /// （Insertトリガーなどを使用して監視専用テーブルにデータを挿入しそちらを監視する。）
+        /// 
+        /// ■元テーブルと監視対象テーブルを別に分ける理由
+        /// 監視対象テーブルが更新されると
+        /// 更新対象データとともにDependecy用の自動生成されたデータも更新される。
+        /// 複数トランザクションから2回以上ずつ更新される場合、
+        /// 更新対象データとDependecy用のデータを別トランザクションが互い違いにつかみ合い
+        /// デッドロックを起こすことがあるため。
+        /// </remarks>
+        private string Sql { get; set; }
+
         /// <summary>
         /// コンストラクタ
         /// </summary>
@@ -35,6 +57,7 @@ namespace SqlDependencySample
             this.ConnectionString ="";
             this.MaxLoopNum = 60;
             this.SleepMillisec = 1000;
+            this.Sql = "select column from dbo.MonitorTable";
         }
 
         /// <summary>
@@ -42,6 +65,9 @@ namespace SqlDependencySample
         /// </summary>
         public void Loop()
         {
+            this.InitSqlDependency();
+            this.SetSqlDependency();
+
             var loopCount = 0;
             while (true)
             {
@@ -52,24 +78,98 @@ namespace SqlDependencySample
                     this.DoSomething();
                 }
 
-                //// ループの停止処理をここに記載
+                if (this.CheckExitLoop())
+                {
+                    break;
+                }
             }
+
+            this.TerminateSqlDependency();
         }
 
         /// <summary>
         /// SqlDependency初期化
         /// </summary>
-        private void Initialize()
+        private void InitSqlDependency()
         {
             SqlDependency.Start(this.ConnectionString);
         }
 
         /// <summary>
-        /// 実処理をここに記載します。
+        /// SqlDependency設定
         /// </summary>
+        private void SetSqlDependency()
+        {
+            using (var conn = new SqlConnection(this.ConnectionString))
+            {
+                conn.Open();
+                using (var command = new SqlCommand(this.Sql, conn))
+                {
+                    command.Notification = null;
+                    var dependency = new SqlDependency(command);
+                    dependency.OnChange += new OnChangeEventHandler(this.OnChangeDependency);
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        // このタイミングで監視が開始されるので
+                        // 下の処理実施中にOnChangeDependencyメソッドが呼び出される場合がある
+                        // DoSomethingメソッドが重複実行不可の場合、何らかの制御が必要
+                        this.DoSomething();
+                    }
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// SqlDependency終了処理
+        /// </summary>
+        private void TerminateSqlDependency()
+        {
+            SqlDependency.Stop(this.ConnectionString);
+        }
+
+        /// <summary>
+        /// 通知イベント発生時に呼び出されるメソッドです。
+        /// 監視設定SQLの実行結果が変化した場合やエラー発生時に呼び出されます。
+        /// </summary>
+        /// <param name="sender">SqlDependencyオブジェクト</param>
+        /// <param name="e">通知イベント情報</param>
+        private void OnChangeDependency(object sender, SqlNotificationEventArgs e)
+        {
+            try
+            {
+                this.DoSomething();
+                var dependency = sender as SqlDependency;
+                dependency.OnChange -= this.OnChangeDependency;
+                this.SetSqlDependency();
+            }
+            catch (Exception)
+            {
+                // ログ出力などのエラー処理を行う
+            }
+        }
+
+        /// <summary>
+        /// 更新を検知した場合に実行すべき処理をここに記載します。
+        /// </summary>
+        /// <remarks>
+        /// 通知を受けた場合、
+        /// 前の通知によって作成されたスレッドがデータを処理してしまい
+        /// 更新対象データがない場合も想定した作りにする必要があります。
+        /// </remarks>
         private void DoSomething()
         {
-            
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// ループを抜けるためのチェック処理を記述します。
+        /// </summary>
+        /// <returns></returns>
+        private bool CheckExitLoop()
+        {
+            throw new NotImplementedException();
         }
     }
 }
